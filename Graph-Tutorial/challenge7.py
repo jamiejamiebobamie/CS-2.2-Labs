6097c97bd404172757873f41ab2ab83aa51dbbe5
jamiejamiebobamie <jmccrory@vt.edu>
2019-07-28 19:16:14 -0700

    added more code

---
+++ Graph-Tutorial/challenge7.py
@@ -0,0 +1,27 @@
+"""
+Chapter 7: How to Win Friends and Influence Users
+
+Google's PageRank algorithm is what they use to show you the most relevant search results for your query.
+Through this, Google influences what you see on that first page every single time you search something
+(and how often are you going past the first page?)
+
+PageRank Your Friends
+
+PageRank is currently implemented using concepts from graph theory,
+assigning scores of "relevance" to links.
+We're going to model that by doing the same thing to our social networks (what,
+you've never ranked your friends before?). This is how social media influence is
+calculated! Let's find out which of our fake friends have the most influence in the network:
+
+Note: For the below challenge, you'll be using a directed weighted graph from
+the starter code, which can be found here
+
+CODE GOES HERE
+
+walk through implementing page rank on the graph, step by step
+Clique through
+
+So now you've ranked your fake friends (do they feel real, yet?), but as with
+any large group of people, smaller groups start to form within the larger ones.
+We can find these cliques through graph theory! One more chapter to go, you got this!
+"""
---
+++ Graph-Tutorial/challenge8.py
@@ -0,0 +1,22 @@
+"""
+Chapter 8: On Wednesdays We Wear Pink
+
+Find “cliques” of friends (small groups of tightly-connected users), etc…
+
+While we are confident that we're not bound by wearing pink on Wednesdays,
+solving clique problems is a whole other issue. These problems tend to be challenging,
+but it's nothing we can't take on!
+
+Clique Discovery
+
+Among other applications, the clique problem can arise in a social network.
+With our social network, a clique will represent a subset of people (nodes) who
+all know each other (share edges), and we can use various algorithms to find these cliques.
+
+Challenge: Write a function that given a graph as input, finds INSERT SOMETHING ABOUT CLIQUES
+
+CODE GOES HERE
+
+walk through clique algorithm, step by step
+Congrats on completing your journey through the Faker Network!
+"""
